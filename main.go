811d779c62d45ab8149914cc8a16a35ee840f9ea
Sebastian Himberger <sebastian@himberger.de>
2018-04-07 14:50:30 +0200

    Added support for stdin

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -22,7 +26,7 @@ var (
 	contentType = kingpin.Flag("content-type", "The content type to assign this object").Default("binary/octet-stream").String()
 	region      = kingpin.Flag("region", "The region to use.").Required().String()
 	bucket      = kingpin.Flag("bucket", "The bucket to upload into.").Required().String()
-	source      = kingpin.Arg("source", "The file to upload.").Required().File()
+	source      = kingpin.Arg("source", "The file to upload.").Required().String()
 	target      = kingpin.Arg("target", "The name of the file.").Required().String()
 )
 
@@ -37,6 +41,18 @@ func main() {
 	// Create an S3 uploader, this uploader allows multipart uploads
 	uploader := s3manager.NewUploader(sess)
 
+	// Read the file or stdin
+	var file io.Reader = nil
+	var err error = nil
+	if *source == "-" {
+		file = bufio.NewReader(os.Stdin)
+	} else {
+		file, err = os.Open(*source)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	// Upload the file to S3
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket:      aws.String(*bucket),
@@ -46,7 +62,7 @@ func main() {
 			"Content-Type": contentType,
 		},
 		ACL:  acl,
-		Body: *source,
+		Body: file,
 	})
 
 	// Check for errors
